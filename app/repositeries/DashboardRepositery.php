c5b004090fdeec78d485b9353a559efea13396b2
Hugo <amaglioh@gmail.com>
2026-08-14 21:20:17 +0200

    feature : Ajout de fonction dans le fichier DashboardRepositery -> ''getAllExpensesByCategories", "modifyExpense", "deleteExpense",

--- app/repositeries/DashboardRepositery.php
+++ app/repositeries/DashboardRepositery.php
@@ -91,6 +91,86 @@ class DashboardRepositery {
         : 0;
     }
 
+    public function getTotalExpenseByCategories(int $userId, int $categoriesId): ?int{
+        $currentDate = new DateTimeImmutable('now');
+        $year = $currentDate->format('Y');
+        $month = $currentDate->format('m');
+
+        $stmt =$this->db->prepare("SELECT SUM(a.amout) AS total_expense
+        FROM actions AS a 
+        INNER JOIN users AS u ON a.id_users = u.id
+        INNER JOIN categories AS c ON a.id_categories = c.id_categories
+        WHERE u.id = :user_id
+        AND a.type ='depense'
+        AND c.id = :id_categories
+        AND YEAR (a.actions_date) = :year
+        AND MONTH (a.actions_date) = :month");
+        
+        $stmt->execute([
+            'user_id' => $userId,
+            'id_categories' => $categoriesId,
+            'year' => $year,
+            'month' => $month
+        ]);
+        
+        $result = $stmt->fetch();
+        
+        return $result && $result['total_expense'] !== null 
+        ? (int) $result['total_expense'] 
+        : 0;
+    }
+
+    function getAllExpensesByCategories(int $userId, int $categoriesId): array {
+        $currentDate = new DateTimeImmutable('now');
+        $year = $currentDate->format('Y');
+        $month = $currentDate->format('m');
+
+        $stmt =$this->db->prepare("SELECT a.id, a.amout, a.action_date, c.name, a.libelle as category_name
+        FROM actions AS a 
+        INNER JOIN users AS u ON a.id_users = u.id
+        INNER JOIN categories AS c ON a.id_categories = c.id_categories
+        WHERE u.id = :user_id
+        AND a.type ='depense'
+        AND c.id = :id_categories
+        AND YEAR (a.actions_date) = :year
+        AND MONTH (a.actions_date) = :month
+        ORDER BY a.action_date DESC");
+        
+        $stmt->execute([
+            'user_id' => $userId,
+            'id_categories' => $categoriesId,
+            'year' => $year,
+            'month' => $month
+        ]);
+        
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    function modifyExpense(int $expenseId, float $amount, string $actionDate, int $idCategories, string $libelle): bool {
+        $sql = "UPDATE actions SET amount = :amount, action_date = :action_date, id_categories = :id_categories, libelle = :libelle WHERE id = :id";
+        
+        $stmt = $this->db->prepare($sql);
+        
+        return $stmt->execute([
+            'id' => $expenseId,    
+            'amount' => $amount,
+            'action_date' => $actionDate,
+            'id_categories' => $idCategories,
+            'libelle' => $libelle
+        ]);
+    }
+
+    public function deleteExpense(int $expenseId): bool {
+        $sql = "DELETE FROM actions WHERE id = :id";
+        
+        $stmt = $this->db->prepare($sql);
+        
+        return $stmt->execute([
+            'id' => $expenseId
+        ]);
+    }
+
+
     public function getAllCategories(): array {
     $sql = "SELECT id_categories, name FROM categories ORDER BY name ASC";
 
